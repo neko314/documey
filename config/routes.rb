0239b2d7f5587217a450a33b709286d97d4af65c
keiko <keiko.cda@gmail.com>
2018-09-03 14:04:49 +0900

    Add login form and change route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-  get 'user_sessions/create'
-  get 'user_sessions/destroy'
+  root :to => 'users#index'
+  resources :user_sessions
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 end
