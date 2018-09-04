3e94edcb944508ffc75e94dab73fe657970d4df6
keiko <keiko.cda@gmail.com>
2018-09-04 18:15:00 +0900

    Add before_action

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,4 +1,6 @@
 class UserSessionsController < ApplicationController
+  skip_before_action :require_login, except: [:destroy]
+
   def new
     @user = User.new
   end
@@ -7,7 +9,7 @@ class UserSessionsController < ApplicationController
     if @user = login(params[:email], params[:password])
       redirect_back_or_to(:users, notice: 'Login successful')
     else
-      flash.now[:alert] = 'Login failed'
+      # flash.now[:alert] = 'Login failed'
       render action: 'new'
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true
   validates :name, presence: true
-  validates :membership_number, format: { with: /\A[A-Z]{3}\d{6}/, message: "shoul be 3upcase characters + 6integers" }, allow_blank: true
+  validates :membership_number, format: { with: /\A[A-Z]{3}\d{6}/, message: "should be 3 upcase characters + 6 integers" }, allow_blank: true
 end
