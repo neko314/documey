50d3d350d1cd2be6e538837780304192bca443bb
keiko <keiko.cda@gmail.com>
2018-09-04 10:05:03 +0900

    Add columns:name&membership_number to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_03_045452) do
+ActiveRecord::Schema.define(version: 2018_09_04_010149) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2018_09_03_045452) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "membership_number"
   end
 
 end
