6204eb761d497d5ff0b65cf67ec14d44e671750f
keiko <keiko.cda@gmail.com>
2018-09-25 16:17:58 +0900

    Add registration participants

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,2 +1,43 @@
-class ParticipantsController < ApplicationController
+class ParticipantsController < SeminarsController
+  def index
+    console
+    @participants = Participant.where(seminar_id: params[:seminar_id]).all
+    @seminar = Seminar.find(params[:seminar_id])
+
+  end
+
+  def show
+    @oarticipant = Participant.find(params[:id])
+  end
+
+  def new
+    console
+  end
+
+  def edit
+  end
+
+  def create
+    @participant = Participant.new(participant_params)
+    @seminar = Seminar.find(params[:seminar_id])
+    @participant.seminar_id = @seminar.id
+    if @participant.save
+      console
+      redirect_to user_seminar_participants_path(@seminar)
+    else
+      redirect_to new_user_seminar_participant_path(@seminar)
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def participant_params
+      params.require(:participant).permit(:name, :membership_number, :email, :seminar_id)
+    end
+
 end
