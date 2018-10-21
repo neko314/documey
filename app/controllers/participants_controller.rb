18179211534a71d18ad63eff9278160271edfe42
keiko <keiko.cda@gmail.com>
2018-10-21 13:01:14 +0900

    Change redirect to, from participants index to seminar

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,5 +1,6 @@
 class ParticipantsController < ApplicationController
   def show
+    @seminar = Seminar.find(params[:seminar_id])
     @participant = Participant.find(params[:id])
   end
 
@@ -8,6 +9,7 @@ class ParticipantsController < ApplicationController
   end
 
   def edit
+    @seminar = Seminar.find(params[:seminar_id])
     @participant = Participant.find(params[:id])
   end
 
@@ -16,25 +18,27 @@ class ParticipantsController < ApplicationController
     @seminar = Seminar.find(params[:seminar_id])
     @participant.seminar_id = @seminar.id
     if @participant.save
-      redirect_to user_seminar_participants_path(@seminar), notice: "create participant"
+      redirect_to user_seminar_path(current_user, @seminar), notice: "create participant"
     else
-      redirect_to edit_user_seminar_participant_path(@seminar), notice: "fail to create participant"
+      redirect_to edit_user_seminar_participant_path(current_user, @seminar), notice: "fail to create participant"
     end
   end
 
   def update
+    @seminar = Seminar.find(params[:seminar_id])
     @participant = Participant.find(params[:id])
     if @participant.update(participant_params)
-      redirect_to user_seminar_participant_path(@participant), notice: "update participant"
+      redirect_to user_seminar_path(current_user, @seminar), notice: "update participant"
     else 
       redirect_to edit_user_seminar_participant_path, notice: "fail to update participant"
     end
   end
 
   def destroy
+    @seminar = Seminar.find(params[:seminar_id])
     @participant = Participant.find(params[:id])
     @participant.destroy
-    redirect_to user_seminar_participants_path(current_user), notice: "delete particepant"
+    redirect_to user_seminar_path(current_user, @seminar), notice: "delete particepant"
   end
 
   private
