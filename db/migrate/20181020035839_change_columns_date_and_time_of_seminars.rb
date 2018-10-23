871f555ec99bf9d3437033a856b762341ae1d015
keiko <keiko.cda@gmail.com>
2018-10-23 23:00:19 +0900

    Delete migration file

--- db/migrate/20181020035839_change_columns_date_and_time_of_seminars.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnsDateAndTimeOfSeminars < ActiveRecord::Migration[5.2]
-  def change
-    change_column :seminars, :start_at, :datetime
-    change_column :seminars, :finish_at, :datetime
-  end
-end
