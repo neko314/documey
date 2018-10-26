661f3c97c5d242c198f3dd61e2f732e1e10acc80
keiko <keiko.cda@gmail.com>
2018-10-26 10:52:13 +0900

    Add report_mail

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,2 +1,6 @@
 class ReportMailer < ApplicationMailer
+  def report_mail
+    @user = params[:user]
+    mail(from: @user.email, subject: '【活動実施報告書】を提出いたします')
+  end
 end
--- test/mailers/previews/report_mailer_preview.rb
+++ test/mailers/previews/report_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/report_mailer
 class ReportMailerPreview < ActionMailer::Preview
-
+  def report_mail
+    ReportMailer.with(user: User.first).report_mail
+  end
 end
