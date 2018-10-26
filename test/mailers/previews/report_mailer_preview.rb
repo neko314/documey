c2d30fd13f6e6f1ebfc35681b70fffea50255b03
keiko <keiko.cda@gmail.com>
2018-10-26 11:14:42 +0900

    Add PDF attachments

--- test/mailers/previews/report_mailer_preview.rb
+++ test/mailers/previews/report_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/report_mailer
 class ReportMailerPreview < ActionMailer::Preview
   def report_mail
-    ReportMailer.with(user: User.first).report_mail
+    ReportMailer.with(user: User.first, seminar: Seminar.first).report_mail
   end
 end
