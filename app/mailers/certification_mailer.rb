cfe2f71cc139e5cac5fafcc0a3f7ba4bc3aec152
keiko <keiko.cda@gmail.com>
2018-10-29 12:00:20 +0900

    Add attachment PDF to certification mail

--- app/mailers/certification_mailer.rb
+++ app/mailers/certification_mailer.rb
@@ -2,6 +2,10 @@ class CertificationMailer < ApplicationMailer
   def certification_mail
     @user = params[:user]
     @seminar = params[:seminar]
+    @participants = @seminar.participants
     mail(from: "#{@user.email}", to: "cert@example.com", subject: "参加証明書の送付")
+    mail.attachments["参加証明書"] = WickedPdf.new.pdf_from_string(
+      render_to_string(template: "participants/certification.pdf.erb")
+    )
   end
 end
