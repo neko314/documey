291e2ef5b34cf6e05918f10463cf95b46773e22b
keiko <keiko.cda@gmail.com>
2018-10-29 17:12:55 +0900

    Change certification PDF generator to be made for each participant

--- app/mailers/certification_mailer.rb
+++ app/mailers/certification_mailer.rb
@@ -4,13 +4,8 @@ class CertificationMailer < ApplicationMailer
     @seminar = params[:seminar]
     @participant = params[:participant]
     mail(from: "#{@user.email}", to: "#{@participant.email}", subject: "参加証明書の送付")
-    # participants = @seminar.participants
-    # participants.each do |p|
-    #   @participant = p
-    #   mail(from: "#{@user.email}", to: "#{@participant.email}", subject: "参加証明書の送付")
-    # end
-      # mail.attachments["参加証明書"] = WickedPdf.new.pdf_from_string(
-      #   render_to_string(template: "participants/certification.pdf.erb")
-      # )
+    mail.attachments["参加証明書"] = WickedPdf.new.pdf_from_string(
+      render_to_string(template: "participants/certification.pdf.erb")
+    )
   end
 end
