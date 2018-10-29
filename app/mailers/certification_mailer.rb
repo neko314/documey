5902d6b2c719d6ce49ee347361f9071d1dc1cb75
keiko <keiko.cda@gmail.com>
2018-10-29 11:36:58 +0900

    Rename 'cert' to 'certification'

--- app/mailers/certification_mailer.rb
+++ app/mailers/certification_mailer.rb
@@ -1,5 +1,5 @@
 class CertificationMailer < ApplicationMailer
-  def cert_mail
+  def certification_mail
     @user = params[:user]
     @seminar = params[:seminar]
     mail(from: "#{@user.email}", to: "cert@example.com", subject: "参加証明書の送付")
--- test/mailers/previews/certification_mailer_preview.rb
+++ test/mailers/previews/certification_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/certification_mailer
 class CertificationMailerPreview < ActionMailer::Preview
-  def cert_mail
-    CertificationMailer.with(user: User.first, seminar: Seminar.first).cert_mail.deliver
+  def certification_mail
+    CertificationMailer.with(user: User.first, seminar: Seminar.first).certification_mail
   end
 end
