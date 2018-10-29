3196f665f3ee1c67437b9d3fdeb8c5a30e5a59f9
keiko <keiko.cda@gmail.com>
2018-10-29 10:56:06 +0900

    Add sinple cert_mail

--- app/mailers/certification_mailer.rb
+++ app/mailers/certification_mailer.rb
@@ -1,2 +1,7 @@
 class CertificationMailer < ApplicationMailer
+  def cert_mail
+    @user = params[:user]
+    @seminar = params[:seminar]
+    mail(from: "#{@user.email}", to: "cert@example.com", subject: "参加証明書の送付")
+  end
 end
--- test/mailers/previews/certification_mailer_preview.rb
+++ test/mailers/previews/certification_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/certification_mailer
 class CertificationMailerPreview < ActionMailer::Preview
-
+  def cert_mail
+    CertificationMailer.with(user: User.first, seminar: Seminar.first).cert_mail.deliver
+  end
 end
