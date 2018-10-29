4a691118335545b1020af2f020340c5804bee8fe
keiko <keiko.cda@gmail.com>
2018-10-29 10:24:08 +0900

    Create certification mailer

---
+++ app/mailers/certification_mailer.rb
@@ -0,0 +1,2 @@
+class CertificationMailer < ApplicationMailer
+end
---
+++ test/mailers/previews/certification_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/certification_mailer
+class CertificationMailerPreview < ActionMailer::Preview
+
+end
