8cfd41e79b27848c2240445cc192c091ba51d559
keiko <keiko.cda@gmail.com>
2018-10-30 18:05:21 +0900

    Change user email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  validates :email, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Emailに使える文字のみ入力してください" }
   validates :name, presence: true
   validates :kana, presence: true
   validates :membership_number, uniqueness: true, allow_blank: true, format: { with: /\A[A-Z]{3}\d{6}\z/, message: "should be 3 upcase characters + 6 integers" }
