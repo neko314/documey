0b8c6c7c0bedf7428caf6857563b65023f1b9e9b
keiko <keiko.cda@gmail.com>
2018-10-30 18:06:45 +0900

    Change seminar validation

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,7 +1,7 @@
 class Participant < ApplicationRecord
   belongs_to :seminar
-  validates :name, presence: true
 
-  validates :membership_number, format: { with: /\A[A-Z]{3}\d{6}\z/, message: "should be 3 upcase characters + 6 integers" }, allow_blank: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Emailに使える文字のみ入力してください" }, allow_blank: true
+  validates :name, presence: true
+  validates :membership_number, uniqueness: true, format: { with: /\A[A-Z]{3}\d{6}\z/, message: "should be 3 upcase characters + 6 integers" }, allow_blank: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "be sure it's correct" }
 end
