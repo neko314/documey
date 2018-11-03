6ed15eff0f4642fef366b9f62200f3f8e39e29d6
keiko <keiko.cda@gmail.com>
2018-11-03 20:12:18 +0900

    Fix user system test

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -9,31 +9,31 @@ class UsersTest < ApplicationSystemTestCase
     visit new_user_path
     fill_in "氏名", with: "test"
     fill_in "フリガナ", with: "テスト"
-    fill_in "Email", with: "test@example.com"
+    fill_in "メールアドレス", with: "test@example.com"
     fill_in "会員番号", with: "ZZZ111111"
     fill_in "パスワード", with: "111"
     fill_in "パスワード（確認用）", with: "111"
-    click_on "登録する"
+    click_on "登録"
     assert_text "ユーザー登録が完了しました"
   end
 
   test "updating a user" do
     visit new_user_session_path
-    fill_in "email", with: @user.email
-    fill_in "password", with: "111111"
-    click_on "ログイン"
-    visit edit_user_path(@user)
-    fill_in "Email", with: "yamadadayo@example.com"
-    click_on "更新する"
+    fill_in "メールアドレス", with: @user.email
+    fill_in "パスワード", with: "111111"
+    click_on("login_button")
+    visit "/users/#{@user.id}/edit"
+    fill_in "メールアドレス", with: "yamadadayo@example.com"
+    click_on "更新"
     assert_text "プロフィールを更新しました"
   end
 
   test "destroying a user" do
     visit new_user_session_path
-    fill_in "email", with: @user.email
-    fill_in "password", with: "111111"
-    click_on "ログイン"
-    visit edit_user_path(@user)
+    fill_in "メールアドレス", with: @user.email
+    fill_in "パスワード", with: "111111"
+    click_on("login_button")
+    visit "/users/#{@user.id}/edit"
     page.accept_confirm do
       click_on "アカウント削除", match: :first
     end
