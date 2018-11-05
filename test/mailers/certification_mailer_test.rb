8b40349e93ba89aa6fbb1be97d660f4d4eb2b778
keiko <keiko.cda@gmail.com>
2018-11-05 18:47:06 +0900

    Add certification mailer test, only text in html

--- test/mailers/certification_mailer_test.rb
+++ test/mailers/certification_mailer_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class CertificationMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:user1)
+    @seminar = seminars(:seminar1)
+    @participant = participants(:participant1)
+  end
+
+  test "certification mail" do
+    # email = CertificationMailer.with(user: @user, seminar: @seminar, participant: @participant).certification_mail
+    email = CertificationMailer.with(user: @user, seminar: @seminar, participant: @participant).certification_mail
+    assert_equal "参加証明書の送付", email.subject
+    assert_equal ["#{@user.email}"], email.from
+    assert_equal ["#{@participant.email}"], email.to
+    # byebug
+    assert_equal read_fixture("certification_mail_html").join, email.html_part.body.to_s
+    # byebug
+    # assert true
+  end
 end
