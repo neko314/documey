0fdb1adaf47a2fe16ffbf4c68b037931ca4cda74
keiko <keiko.cda@gmail.com>
2018-11-06 17:06:36 +0900

    Move action of certification mail send to certification mails controller

--- app/controllers/certification_mails_controller.rb
+++ app/controllers/certification_mails_controller.rb
@@ -1,4 +1,15 @@
 class CertificationMailsController < ApplicationController
   def show
+    @seminar = Seminar.find(params[:seminar_id])
+  end
+
+   def create
+    @user = current_user
+    @seminar = Seminar.find(params[:seminar_id])
+    @seminar.participants.each do |p|
+      @participant = p
+      CertificationMailer.with(user: @user, seminar: @seminar, participant: @participant).certification_mail.deliver
+    end
+    redirect_to [current_user, @seminar], notice: t("Sent_certification_successfully")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
       get 'send_report' => 'report_mails#create'
       get 'certification' => 'participants#certification'
       get 'certficitaion_preview' => 'certification_mails#show'
-      get 'cert_mail' => 'participants#send_certification_mail'
+      get 'send_certficitaion' => 'certification_mails#create'
     end 
   end
   
