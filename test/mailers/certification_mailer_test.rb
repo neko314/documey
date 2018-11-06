7a29638ebf0f1cef73f314928cd7146fed7f2968
keiko <keiko.cda@gmail.com>
2018-11-06 10:36:32 +0900

    Add text mail test to certification mailer test

--- test/mailers/certification_mailer_test.rb
+++ test/mailers/certification_mailer_test.rb
@@ -8,14 +8,11 @@ class CertificationMailerTest < ActionMailer::TestCase
   end
 
   test "certification mail" do
-    # email = CertificationMailer.with(user: @user, seminar: @seminar, participant: @participant).certification_mail
     email = CertificationMailer.with(user: @user, seminar: @seminar, participant: @participant).certification_mail
     assert_equal "参加証明書の送付", email.subject
     assert_equal ["#{@user.email}"], email.from
     assert_equal ["#{@participant.email}"], email.to
-    # byebug
     assert_equal read_fixture("certification_mail_html").join, email.html_part.body.to_s
-    # byebug
-    # assert true
+    assert_equal read_fixture("certification_mail_text").join, email.text_part.body.to_s
   end
 end
