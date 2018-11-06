b6fb7c077e5eb3b0d1181dc83bbf6473b93de897
keiko <keiko.cda@gmail.com>
2018-11-06 12:46:08 +0900

    Add report mailer test

--- test/mailers/report_mailer_test.rb
+++ test/mailers/report_mailer_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ReportMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:user1)
+    @seminar = seminars(:seminar1)
+  end
+
+  test "report mail" do
+    email = ReportMailer.with(user: @user, seminar: @seminar).report_mail.deliver
+    assert_equal "【活動実施報告書】を提出いたします", email.subject
+    assert_equal ["#{@user.email}"], email.from
+    assert_equal ["keiko.cda@gmail.com"], email.to
+    assert_equal read_fixture("report_mail_html").join, email.html_part.body.to_s
+    assert_equal read_fixture("report_mail_text").join, email.text_part.body.to_s
+  end
 end
