40305e880f73efb87caf70ed0bba4fe016df7f75
keiko <keiko.cda@gmail.com>
2018-11-06 21:42:47 +0900

    Add certification mail system test

--- test/system/certification_mails_test.rb
+++ test/system/certification_mails_test.rb
@@ -12,13 +12,14 @@ class CertificationMailsTest < ApplicationSystemTestCase
     fill_in "パスワード", with: "111111"
     click_on("login_button")
     visit "/users/#{@user.id}/seminars/#{@seminar.id}"
-    # byebug
-    click_link "参加証明書のメールを作成"
     sleep 5
-    # assert_equal "/users/#{@user.id}/seminars/#{@seminar.id}/report_preview", current_path
-    # assert_text "プレビュー"
-    # click_link "メール送信"
-    # sleep 5
-    # assert_text "加証明書を送付しました"
+    click_link "参加証明書のメールを作成"
+    sleep 10
+    assert_equal "/users/#{@user.id}/seminars/#{@seminar.id}/certification_preview", current_path
+    assert_text "プレビュー"
+    click_link "メール送信"
+    sleep 10
+    byebug
+    assert_text "参加証明書を送付しました"
   end
 end
