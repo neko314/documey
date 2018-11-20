9b06a7f5f83416aad0429b1e9cf0dfa012acd880
keiko <keiko.cda@gmail.com>
2018-11-20 15:44:25 +0900

    Update certification mailer and pdf

--- app/mailers/certification_mailer.rb
+++ app/mailers/certification_mailer.rb
@@ -5,7 +5,7 @@ class CertificationMailer < ApplicationMailer
     @participant = params[:participant]
     mail(from: "#{@user.email}", to: "#{@participant.email}", subject: "参加証明書の送付")
     mail.attachments["参加証明書"] = WickedPdf.new.pdf_from_string(
-      render_to_string(template: "participants/certification.pdf.erb")
+      render_to_string(template: "certifications/show.pdf.erb")
     )
   end
 end
